1bd8414b425d996cfa7d42fb5d0c5ed9cf4e8ac8
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-03-20 18:25:02 +0530

    balance-add

--- src/Components/SwitchAccount.js
+++ src/Components/SwitchAccount.js
@@ -4,14 +4,22 @@ import Web3 from 'web3';
 const AccountSwitcher = () => {
     const [accounts, setAccounts] = useState([]);
     const [currentAccountIndex, setCurrentAccountIndex] = useState(0);
+    const [balances, setBalances] = useState([]);
 
     useEffect(() => {
         if (window.ethereum) {
             const web3 = new Web3(window.ethereum);
             window.ethereum.enable().then(() => {
-                web3.eth.getAccounts().then((accounts) => {
+                web3.eth.getAccounts().then(async (accounts) => {
                     setAccounts(accounts);
                     setCurrentAccountIndex(0);
+                    const accountBalances = await Promise.all(
+                        accounts.map(async (account) => {
+                            const balance = await web3.eth.getBalance(account);
+                            return web3.utils.fromWei(balance, 'ether');
+                        })
+                    );
+                    setBalances(accountBalances);
                 });
                 window.ethereum.on('accountsChanged', (newAccounts) => {
                     setAccounts(newAccounts);
@@ -31,6 +39,7 @@ const AccountSwitcher = () => {
     return (
         <div>
             <h1>Current Account: {accounts[currentAccountIndex]}</h1>
+            <h1>Balance: {balances[currentAccountIndex]} ETH</h1>
             {accounts.length > 0 && (
                 <button onClick={switchAccount}>
                     Switch Account
