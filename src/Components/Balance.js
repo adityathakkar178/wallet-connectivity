546482d4fa23be90b6bf15fc462eb1f240c52ec8
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-03-27 17:17:05 +0530

    restructure-basic

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,49 +1,28 @@
-import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 
 const Balance = ({ contract }) => {
     const [address, setAddress] = useState('');
     const [balance, setBalance] = useState('');
 
     const handleChange = (e) => {
-        setAddress(e.target.value);
+        setAddress(e.target.value)
     };
 
     const getBalance = () => {
-        contract
-            .balanceOf(address)
-            .then((balance) => {
-                setBalance(balance.toString());
-            })
-            .catch((err) => {
-                console.log('Error fetching balance', err);
-            });
+        contract.balanceOf(address).then((balance) => {
+            setBalance(balance.toString());
+        }).catch((err) => {
+            console.log("Error fetching balance", err);
+        });
     };
-    return (
-        <div>
-            <h1>Check Balance</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicAddress">
-                    <Form.Label>Enter address</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter address"
-                        value={address}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                <Button variant="primary" onClick={getBalance}>
-                    Get balance
-                </Button>
-            </Form>
-            {balance !== null && (
-                <h1>
-                    Token Balance of {address}: {balance}
-                </h1>
-            )}
-        </div>
-    );
+    return (<div>
+        <h1>Check Balance</h1>
+        <label>Enter address
+            <input type="test" value={address} onChange={handleChange}/>
+        </label>
+        <button onClick={getBalance}>Get balance</button>
+        {balance !== null && <h1>Token Balance of {address}: {balance}</h1>}
+    </div>);
 };
 
 export default Balance;
