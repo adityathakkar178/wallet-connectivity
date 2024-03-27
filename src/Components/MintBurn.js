9055188d814d4cfc3888d0835bcc35af7a89276d
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-03-27 10:27:43 +0530

    adding-data-to-mongodb

--- src/Components/MintBurn.js
+++ src/Components/MintBurn.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const MintBurn = ({ contract, updateTotalSupply }) => {
     const [address, setAddress] = useState('');
@@ -18,15 +19,22 @@ const MintBurn = ({ contract, updateTotalSupply }) => {
         setBurnAmount(e.target.value);
     };
 
-    const mintTokens = () => {
+    const mintTokens = async () => {
         contract
             .mint(address, amount)
             .then((transaction) => {
+                axios.post('http://localhost:3003/mint', {
+                    address,
+                    amount,
+                    transactionHash: transaction.hash,
+                });
+            })
+            .then(() => {
                 setTransaction(transaction.hash);
                 updateTotalSupply();
             })
             .catch((err) => {
-                console.error('Error Minitng Tokens', err);
+                console.error('Error Minting Tokens', err);
             });
     };
 
